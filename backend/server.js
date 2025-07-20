5f35d08f3251614e953abf99f6568b95090c1b33
Syed Haris Shah <syedharryshah1@gmail.com>
2025-07-20 15:22:09 +0500

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,13 @@ cloudinary.v2.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_API_SECRET,
+})import app from "./app.js";
+import cloudinary from "cloudinary"
+
+cloudinary.v2.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
 })
 
 // Basic health check endpoint
@@ -36,3 +43,7 @@ app.use((err, req, res, next) => {
 
 export default app;
 // No app.listen() here for Vercel compatibility
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}`);
+});
