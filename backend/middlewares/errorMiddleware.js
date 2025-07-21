88f86d8c8bef7296e7161d98e28823798925bc48
Syed Haris Shah <syedharryshah1@gmail.com>
2025-07-21 15:34:00 +0500

    modified backend

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -38,7 +38,7 @@ export const errorMiddleware = (err, req, res, next) => {
     return res.status(err.statusCode).json({
         success: false,
         message: errorMessage,
-        ...(isDev && { stack: err.stack })
+        stack: err.stack
     });
 };
 
